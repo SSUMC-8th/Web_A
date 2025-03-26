d300e1f584efecda1cdddba988847f86d7ecf7f4
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-03-26 18:32:03 +0900

    feat : Provider 씌워주기

---
+++ Week2/src/main.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren, StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ToDoProvider } from "./context/todoContext.tsx";
+import { ThemeProvider } from "./context/ThemeProvider.tsx";
+
+export const AppProvider = ({ children }: PropsWithChildren) => (
+  <ThemeProvider>
+    <ToDoProvider>{children}</ToDoProvider>
+  </ThemeProvider>
+);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </StrictMode>
+);
