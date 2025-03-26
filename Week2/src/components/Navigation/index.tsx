ae46d9f5c76524e0495c58cdc9d1addd2db41607
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-03-26 18:35:29 +0900

    UI : NavigationBar 구현 #12

---
+++ Week2/src/components/Navigation/index.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import { THEME, useTheme } from "../../context/ThemeProvider";
+import ThemeToggleButton from "./ThemeToggleButton";
+
+const Navbar = () => {
+  const { theme } = useTheme();
+
+  const isLightMode = theme === THEME.LIGHT;
+
+  return (
+    <nav
+      className={clsx(
+        "p-4 w-full flex justify-center items-center",
+        isLightMode ? "bg-alceblue" : "bg-gray-800"
+      )}
+    >
+      <ThemeToggleButton />
+    </nav>
+  );
+};
+
+export default Navbar;
