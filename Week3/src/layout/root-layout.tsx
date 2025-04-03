5f0b817f1b4e73970582c366d6c8a2837e7bc484
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-04-03 17:30:03 +0900

    Layout 구현 #14

--- Week3/src/layout/root-layout.tsx
+++ Week3/src/layout/root-layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navigation";
 
 const RootLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
