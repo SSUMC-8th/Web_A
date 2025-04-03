ca830ae0fcbd1050d4d11f3c89422533e4e74dc1
Starving Orange <kyungjjoo@gmail.com>
2025-04-03 21:51:17 +0900

    [Week3/mission] Tailwind CSS, React Router, API 통신 완전 정복

---
+++ gang/Week3/Movie/src/Pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div>
+      <p className='text-lg text-gray-700'>나만 아니면 돼에에에에에</p>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ gang/Week3/Movie/src/types/movie.ts
@@ -0,0 +1,45 @@
+export type Movie = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type MovieBody = {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+};
+
+export type MovieInfo = {
+  id: number;
+  title: string;
+  overview: string;
+  runtime: number;
+  release_date: string;
+  backdrop_path: string;
+  poster_path:string;
+  tagline:string;
+};
+
+export type Credit = {
+    id:number;
+    name:string;
+    profile_path:string;
+    character:string;
+}
+
+export type CreditBody = {
+    cast: Credit[];  
+}
