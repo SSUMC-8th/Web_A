4626b48d89aa63c67766f3bdf15305be1ceeacb1
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-04-06 02:57:18 +0900

    feat : SignUpNextBtn 구현 #20

---
+++ Week4_2/src/components/Buttons/SignUpNextButton/index.tsx
@@ -0,0 +1,34 @@
+import { useFormContext } from "react-hook-form";
+
+type Props = {
+  onNext: () => void;
+  fieldsToValidate: string[];
+};
+
+const SignUpNextButton = ({ onNext, fieldsToValidate }: Props) => {
+  const {
+    trigger,
+    formState: { errors },
+    watch,
+  } = useFormContext();
+
+  const values = watch();
+  const isInvalid = fieldsToValidate.some(
+    (field) => !values[field] || !!errors[field]
+  );
+
+  return (
+    <button
+      onClick={async () => {
+        const valid = await trigger(fieldsToValidate);
+        if (valid) onNext();
+      }}
+      disabled={isInvalid}
+      className="bg-white text-black py-2 rounded w-[300px] disabled:bg-gray-600"
+    >
+      다음
+    </button>
+  );
+};
+
+export default SignUpNextButton;
