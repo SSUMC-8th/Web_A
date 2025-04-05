b60d2937977391aa36f1e3ee3b03c65a632770d0
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-04-06 02:44:31 +0900

    feat: LoginPage 구현 #20

--- Week4_2/src/pages/LoginPage.tsx
+++ Week4_2/src/pages/LoginPage.tsx
@@ -1,5 +1,67 @@
+import { postSignIn } from "../api/auth";
+import GoogleLoginButton from "../components/Buttons/GoogleLoginButton";
+import Divider from "../components/Divider";
+import LoginForm from "../components/Login/LoginForm";
+import LoginTitle from "../components/Titles/LoginTitle";
+import { LOCAL_STORAGE_KEY } from "../constants/key";
+import useForm from "../hook/useForm";
+import { useLocalStorage } from "../hook/useLocalStorage";
+import { UserSignInformation, validateLogin } from "../utils/validate";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  const { setItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+  const { values, errors, touched, getInputProps } =
+    useForm<UserSignInformation>({
+      initialValue: {
+        email: "",
+        password: "",
+      },
+      validate: validateLogin,
+    });
+
+  const handleSubmit = async () => {
+    try {
+      const { data } = await postSignIn(values);
+      setItem(data.accessToken);
+      console.log(data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  // 오류가 하나라도 있거나, 입력값이 비어 있으면 버튼을 비활성화
+  // some -> 하나라도 해당하면 true 를 반환
+  // every -> 전부가 해당해야 true 를 반환
+  // Object.values = 객체의 "값들만" 배열로 반환
+  // 여기선 [email 값, password 값]를 의미할듯
+  const isDisabled =
+    Object.values(values).some((v) => v === "") ||
+    Object.values(errors || {}).some((e) => e !== "");
+
+  // {...getInputProps('email')}
+  /*
+  value={}
+  onChange={}
+  onBlur={}
+  */
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full gap-4 bg-black">
+      <LoginTitle text="로그인" />
+
+      <GoogleLoginButton />
+
+      <Divider />
+
+      <LoginForm
+        getInputProps={getInputProps}
+        errors={errors}
+        touched={touched}
+        isDisabled={isDisabled}
+        onSubmit={handleSubmit}
+      />
+    </div>
+  );
 };
 
 export default LoginPage;
