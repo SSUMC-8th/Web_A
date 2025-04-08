04669dc26c6dd25f616ee5945e17c3907f16d928
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-04-08 09:29:25 +0900

    UI: 간단한 myPage 구현 #20

--- Week4_2/src/pages/Mypage.tsx
+++ Week4_2/src/pages/Mypage.tsx
@@ -18,9 +18,14 @@ const Mypage = () => {
   }, []);
 
   return (
-    <div className="bg-black">
-      <img src="http://localhost:8000/uploads/1743869104638-346327669.png" />
-      <div className="text-red-500 text">{data?.data.name}</div>
+    <div className="bg-black h-full flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center">
+        <img
+          className="w-100 h-100 rounded-full object-cover mb-4"
+          src="http://localhost:8000/uploads/1743869104638-346327669.png"
+        />
+      </div>
+      <div className="text-blue-500 text-5xl">{data?.data.name}</div>
     </div>
   );
 };
