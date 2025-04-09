7ac347b08bbd6ab3895a3359b80ba15ba0282b3a
JeonSuna <jsa020910@gmail.com>
2025-04-09 09:14:16 +0900

    ✨ feat: email,password 유효성검사

--- sona/week4/mission2/src/pages/Login.tsx
+++ sona/week4/mission2/src/pages/Login.tsx
@@ -1,7 +1,28 @@
 import { useNavigate } from "react-router-dom";
+import useForm from "../hooks/useForm";
+import { UserSigninInformation, validateSignin } from "../utils/validate";
 
 export default function Login() {
   const navigate = useNavigate();
+
+  const { values, errors, touched, getInputProps } =
+    useForm<UserSigninInformation>({
+      initialValue: {
+        email: "",
+        password: "",
+      },
+      validate: validateSignin,
+    });
+
+  const handleSubmit = () => {
+    if (!errors.email && !errors.password) {
+      alert("로그인에 성공하였습니다");
+      // navigate("/home") or 서버 요청
+    } else {
+      alert("로그인에 실패하였습니다");
+    }
+    console.log(values);
+  };
 
   return (
     <div className="flex flex-col items-center justify-center  max-w-[300px]  m-auto px-2">
@@ -20,18 +41,45 @@ export default function Login() {
         <span className="text-white text-sm">OR</span>
         <div className="border-t border-white grow"></div>
       </div>
-      <input
-        type="email"
-        placeholder="이메일을 입력하세요"
-        className="inputField"
-      />
-      <input
-        type="password"
-        placeholder="비밀번호를 입력하세요"
-        className="inputField"
-      />
-
-      <button className="text-sm">로그인</button>
+      <div className="relative w-full">
+        <input
+          type="email"
+          placeholder="이메일을 입력하세요"
+          className={`inputField ${
+            errors.email && touched.email ? "border-red-400" : "border-gray-300"
+          }`}
+          {...getInputProps("email")}
+        />
+        {errors.email && touched.email && (
+          <div className=" text-red-500 text-[10px] py-1 absolute top-[38px] left-1">
+            {errors.email}
+          </div>
+        )}
+      </div>
+      <div className="relative w-full">
+        <input
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+          className={`inputField ${
+            errors.password && touched.password
+              ? "border-red-400 "
+              : "border-gray-300"
+          }`}
+          {...getInputProps("password")}
+        />
+        {errors.password && touched.password && (
+          <div className="text-red-500 text-[10px] py-1 absolute top-[38px] left-1">
+            {errors.password}
+          </div>
+        )}
+      </div>
+      <button
+        className="text-sm bg-gray-800 w-full rounded-sm"
+        type="button"
+        onClick={handleSubmit}
+      >
+        로그인
+      </button>
     </div>
   );
 }
