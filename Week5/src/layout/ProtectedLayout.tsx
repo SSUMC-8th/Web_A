47d2c5fcd998fe96722e66c86930f29261cfb06a
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-05-01 20:15:07 +0900

    feat: protectedLayout 구현 #29

--- Week5/src/layout/ProtectedLayout.tsx
+++ Week5/src/layout/ProtectedLayout.tsx
@@ -1,5 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/TokenContext/useAuth";
+import Navbar from "../components/Navigation";
+import Footer from "../components/Footer";
 
 const ProtectedLayout = () => {
   const { isLoggedIn } = useAuth();
@@ -8,7 +10,21 @@ const ProtectedLayout = () => {
     return <Navigate to="/login" replace />;
   }
 
-  return <Outlet />;
+  return (
+    <div className="h-dvh flex flex-col">
+      <nav>
+        <Navbar />
+      </nav>
+
+      <main className="flex-1">
+        <Outlet />
+      </main>
+
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default ProtectedLayout;
