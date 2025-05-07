22850be9a35199ecaefa7bdf22368881a76ee37d
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-05-07 13:31:35 +0900

    UI: layout 변경 #34

--- Week6/src/layout/root-layout.tsx
+++ Week6/src/layout/root-layout.tsx
@@ -2,20 +2,29 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navigation";
 import Footer from "../components/Footer";
 
+import { useState } from "react";
+import Sidebar from "../components/SideBar/SideBar";
+
 const RootLayout = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="h-dvh flex flex-col">
-      <nav>
-        <Navbar />
-      </nav>
+    <div className="h-screen flex flex-col bg-black">
+      {/* 상단 네비게이션 바 */}
+      <Navbar onSidebarToggle={() => setIsOpen(!isOpen)} />
 
-      <main className="flex-1">
-        <Outlet />
-      </main>
+      {/* 하단 전체 영역: 사이드바 + 메인 콘텐츠 */}
+      <div className="flex flex-1 overflow-x-hidden">
+        {/* 왼쪽 사이드바 */}
+        <Sidebar isOpen={isOpen} onClose={() => setIsOpen(false)} />
 
-      <footer>
-        <Footer />
-      </footer>
+        {/* 메인 콘텐츠 */}
+        <main className="flex-1 bg-black overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+
+      <Footer />
     </div>
   );
 };
