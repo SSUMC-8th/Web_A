6a543762c2e04abbd208e398cc4ea9dde6c44a11
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-05-07 13:29:08 +0900

    refactor: query_retry defalut 3으로 설정 #34

--- Week6/src/main.tsx
+++ Week6/src/main.tsx
@@ -5,7 +5,13 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3, // 쿼리 요청이 실패했을 때 자동으로 재시도할 횟수를 지정합니다.
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
