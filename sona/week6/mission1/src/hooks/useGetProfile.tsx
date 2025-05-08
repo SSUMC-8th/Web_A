4d0ee153cf7337d4e78d8311ae2564d0526468c7
JeonSuna <jsa020910@gmail.com>
2025-05-08 16:17:19 +0900

    ✨ feat: nav 2차 완성

--- sona/week6/mission1/src/hooks/useGetProfile.tsx
+++ sona/week6/mission1/src/hooks/useGetProfile.tsx
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosInstance from "../apis/axios";
+import { useAuth } from "../context/AuthContext";
 
 export default function useGetProfile() {
+  const { accessToken } = useAuth();
   const { data } = useQuery({
     queryKey: ["user"],
     queryFn: () => axiosInstance.get(`/v1/users/me`),
+    enabled: !!accessToken,
   });
   const user = data?.data;
   return { user };
--- sona/week6/mission1/src/pages/Navbar.tsx
+++ sona/week6/mission1/src/pages/Navbar.tsx
@@ -6,10 +6,14 @@ import useGetProfile from "../hooks/useGetProfile";
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
-  const { logout, accessToken } = useAuth();
+  // const [isTrue, setISTrue] = useState(false);
 
+  const { accessToken } = useAuth();
+  // if (accessToken) {
+  //   setISTrue((item) => !item);
+  // }
   const { user } = useGetProfile();
-  console.log(user);
+  // // console.log(user);
   return (
     <>
       <nav className="flex gap-2 justify-between py-3 px-4 border-b border-gray-500">
@@ -25,11 +29,8 @@ export default function NavBar() {
         <div className="flex text-[18px] font-bold gap-2 items-center">
           {accessToken ? (
             <>
-              <div>{user.data.name}님 반갑습니다</div>
-              <button
-                className="hover:text-amber-500 pt-[2px] "
-                onClick={logout}
-              >
+              <div>{user?.data.name}님 반갑습니다</div>
+              <button className="hover:text-amber-500 pt-[2px] ">
                 로그아웃
               </button>
             </>
