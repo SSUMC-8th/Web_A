452c515c9a534c4c05a45354d7d5c1b0992f6199
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-05-16 00:31:28 +0900

    logout, login mutate

--- Week7/src/context/TokenContext/AuthProvider.tsx
+++ Week7/src/context/TokenContext/AuthProvider.tsx
@@ -1,27 +1,20 @@
-import { PropsWithChildren, useState } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { RequestSignInDto } from "../../types/auth";
 import { useLocalStorage } from "../../hook/useLocalStorage";
 import { LOCAL_STORAGE_KEY } from "../../constants/key";
-import { postSignIn, postSignOut } from "../../api/Post/auth";
 import { AuthContext } from "./AuthContext";
-import RoutePaths from "../../router/routePaths";
+import { useSignIn } from "../../hook/mutations/useLogin";
+import { useSignOut } from "../../hook/mutations/useLogOut";
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
-  const {
-    getItem: getAccessTokenFromStorage,
-    setItem: setAccessTokenInStorage,
-    removeItem: removeAccessTokenFromStorage,
-  } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+  const { getItem: getAccessTokenFromStorage } = useLocalStorage(
+    LOCAL_STORAGE_KEY.accessToken
+  );
 
-  const {
-    getItem: getRefreshTokenFromStorage,
-    setItem: setRefreshTokenInStorage,
-    removeItem: removeRefreshTokenFromStorage,
-  } = useLocalStorage(LOCAL_STORAGE_KEY.refreshToken);
+  const { getItem: getRefreshTokenFromStorage } = useLocalStorage(
+    LOCAL_STORAGE_KEY.refreshToken
+  );
 
-  // Lazy initalization
-  // useState(() => getAccessTokenFromStorage())
-  // Not => useState(getAccessTokenFromStorage()) or useState(getAccessTokenFromStorage)
   const [accessToken, setAccessToken] = useState<string | null>(() =>
     getAccessTokenFromStorage()
   );
@@ -29,45 +22,23 @@ export const AuthProvider = ({ children }: PropsWithChildren) => {
     getRefreshTokenFromStorage()
   );
 
+  // 로그인 상태 동기화
+  useEffect(() => {
+    setAccessToken(getAccessTokenFromStorage());
+    setRefreshToken(getRefreshTokenFromStorage());
+  }, [getAccessTokenFromStorage, getRefreshTokenFromStorage]);
+
   const isLoggedIn = !!accessToken;
 
-  const login = async (signInData: RequestSignInDto) => {
-    try {
-      const { data } = await postSignIn(signInData);
-      if (data) {
-        const newAccessToken = data.accessToken;
-        const newRefreshToken = data.refreshToken;
+  const { mutateAsync: signIn } = useSignIn();
+  const { mutateAsync: signOut } = useSignOut();
 
-        setAccessTokenInStorage(newAccessToken);
-        setRefreshTokenInStorage(newRefreshToken);
-
-        setAccessToken(newAccessToken);
-        setRefreshToken(newRefreshToken);
-
-        alert("로그인 성공");
-        window.location.href = RoutePaths.MYPAGE;
-      }
-    } catch (error) {
-      console.error("로그인 오류", error);
-      alert("로그인에 실패했습니다. 다시 시도해주세요.");
-    }
+  const login = async (dto: RequestSignInDto) => {
+    await signIn(dto);
   };
 
   const logout = async () => {
-    try {
-      await postSignOut();
-      removeAccessTokenFromStorage();
-      removeRefreshTokenFromStorage();
-
-      setAccessToken(null);
-      setRefreshToken(null);
-
-      alert("로그아웃 성공");
-      window.location.href = "/";
-    } catch (error) {
-      console.error("로그아웃 오류", error);
-      alert("로그아웃에 실패했습니다. 다시 시도해주세요.");
-    }
+    await signOut();
   };
 
   return (
